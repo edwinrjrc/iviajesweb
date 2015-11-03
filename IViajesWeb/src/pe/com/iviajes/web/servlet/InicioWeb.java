3df5c1933cbe203c7340f04ad7babcb83a260241
edwinrjrc <edwinrjrc@gmail.com>
2015-11-03 17:52:30 -0500

    Inicio proyecto web IViajes

---
+++ IViajesWeb/src/pe/com/iviajes/web/servlet/InicioWeb.java
@@ -0,0 +1,48 @@
+package pe.com.iviajes.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InicioWeb
+ */
+@WebServlet(loadOnStartup=1, urlPatterns={"/InicioWeb"})
+public class InicioWeb extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InicioWeb() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		servicio(request, response);
+	}
+
+	private void servicio(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		servicio(request, response);
+	}
+
+}
