92faf9b5fe9e4240a9b1d3fa1488282f007e7501
Edwin <edwinrjrc@gmail.com>
2016-05-15 22:22:04 -0500

    pagina web

--- IViajesWeb/WebContent/resources/js/controladorweb.js
+++ IViajesWeb/WebContent/resources/js/controladorweb.js
@@ -9,14 +9,17 @@ portalinnova.controller('PortalInnovaCtrl', function($scope) {
 
 	$scope.portal.redesSociales = "redes sociales facebook, dfsdfk";
 	
+	$scope.imagen1 = "Mancora Sensasional";
+	$scope.imagen2 = "Trujillo Fantastico";
+	$scope.imagen3 = "San Andres de Lujo";
 
 });
 
-portalinnova.controller('PortalInnovaCtrl', [ '$scope', '$http',
+/*portalinnova.controller('PortalInnovaCtrl', [ '$scope', '$http',
 		function($scope, $http) {
 			$http.get('phones/phones.json').success(function(data) {
 				$scope.phones = data;
 			});
 
 			$scope.orderProp = 'age';
-		}]);
+		}]);*/
